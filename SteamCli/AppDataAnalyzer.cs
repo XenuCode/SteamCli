737ea3eac54fe814825e2ccbb01fac4d7fffe9e9
Xenunxenunowski <blacksidepolska@gmail.com>
2022-05-25 20:57:15 +0200

    Initial commit

---
+++ SteamCli/AppDataAnalyzer.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace SteamCli;
+
+public class AppDataAnalyzer
+{
+    private App applist;
+    public void AnalizeJson()
+    {
+        if (File.Exists("gamelistdata.json"))
+        {
+            applist = JsonConvert.DeserializeObject<App>(File.ReadAllText("gamelistdata.json"));
+        }
+        else
+        {
+            Console.WriteLine("No gamelistdata.json file found, do you want to refresh it ?");
+            string resp = Console.ReadLine();
+            if (resp.Contains("Y") || resp.Contains("y"))
+            {
+                RefreshGameList();
+            }
+            else
+            {
+                Console.WriteLine("List not updated, user declined mandatory update of gamelistdata.json");
+                return;
+            }
+        }
+    }
+    public void RefreshGameList()
+    {
+        try
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://api.steampowered.com/ISteamApps/GetAppList/v2/", "gamelistdata.json");
+            applist = JsonConvert.DeserializeObject<App>(File.ReadAllText("gamelistdata.json"));
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Failure could not refresh game list");
+            Console.Error.NewLine = e.Message;
+            return;
+        }
+        Console.WriteLine("Successfully refreshed game list"); 
+    }
+    
+}
+
+public class Applist
+{
+    public Game[] apps;
+}
+public class Game
+{
+    [JsonProperty(PropertyName = "name")]public string name;
+    public int appid;
+}
+
+public class App
+{
+    public Applist applist;
+}
---
+++ SteamCli/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using FuzzySharp;
+using Process = System.Diagnostics.Process;
+
+namespace SteamCli {
+      class Program
+      {
+          public const string VERSION="0.0.2";
+           static void Main(string[] args)
+           {
+               AppDataAnalyzer appDataAnalyzer = new AppDataAnalyzer();
+               Console.WriteLine("SteamCli "+VERSION);
+            Console.WriteLine("\n# Type !help to view help menu");
+            Console.WriteLine("# !run \"gamename\"          runs a game");
+            Console.WriteLine("# !update \"gamename\"       updates a game");
+            /*
+            try
+            {
+                Process.Start("nohup","steam -no-browser steam://open/minigameslist");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            try
+            {
+                Process.Start("nohup","steam steam://rungameid/304930");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            */
+            appDataAnalyzer.RefreshGameList();
+           }
+      }
+};
