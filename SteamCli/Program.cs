7681c4b492f57ebfdb3a039b7926065e44e1230a
Xenu <xenucode@gmail.com>
2023-07-26 17:57:31 +0200

    Bumped .NET to 7.0 and added toast notifications

--- SteamCli/Program.cs
+++ SteamCli/Program.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using DesktopNotifications;
+using DesktopNotifications.Apple;
+using DesktopNotifications.FreeDesktop;
+using DesktopNotifications.Windows;
 using FuzzySharp;
 using Process = System.Diagnostics.Process;
 
 namespace SteamCli {
       class Program
       {
+          private static INotificationManager CreateManager()
+          {
+              if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+              {
+                  return new FreeDesktopNotificationManager();
+              }
+   
+              if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+              {
+                  return new WindowsNotificationManager();
+              }
+
+              if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+              {
+                  return new AppleNotificationManager();
+              }
+
+              throw new PlatformNotSupportedException();
+          }
           public const string VERSION="0.0.2";
-           static void Main(string[] args)
+           static async Task Main(string[] args)
            {
+               using var manager = CreateManager();
+               await manager.Initialize();
+
+               manager.NotificationActivated += ManagerOnNotificationActivated;
+               manager.NotificationDismissed += ManagerOnNotificationDismissed;
+
                AppDataAnalyzer appDataAnalyzer = new AppDataAnalyzer();
                SteamInteractions steamInteractions = new SteamInteractions();
                string command;
@@ -17,6 +47,7 @@ namespace SteamCli {
                Console.Clear();
                Console.WriteLine("SteamCli "+VERSION);
                Console.WriteLine("\n# Type                      !help to view help menu");
+               Console.WriteLine("# !start                      starts steam in smoool mode");
                Console.WriteLine("# !run \"gamename\"           runs a game");
                Console.WriteLine("# !update \"gamename\"        updates a game");
                Console.WriteLine("# !refresh                    downloads and reloads steam game list");
@@ -25,18 +56,49 @@ namespace SteamCli {
                for (;;) 
                {
                    command =Console.ReadLine();
+                   if (command.Contains("!start"))
+                   {    
+                       steamInteractions.RunSteam();
+                       var notification = new Notification
+                       {
+                           Title = "SteamCLI",
+                           Body = "Starting Steam in small mode"
+                       };
+                       await manager.ShowNotification(notification);
+
+                   }
                    if (command.Contains("!run"))
                    {
-                       steamInteractions.RunGame(appDataAnalyzer.FindMatch(command.Substring(command.IndexOf("!run")+ 5,command.Length-5))[0]);
+                       var result =
+                           appDataAnalyzer.FindMatch(command.Substring(command.IndexOf("!run") + 5, command.Length - 5))[0];
+                       steamInteractions.RunGame(result);
+                       var notification = new Notification
+                       {
+                           Title = "SteamCLI",
+                           Body = $"Running {result.name}"
+                       };
+                       await manager.ShowNotification(notification);
                    }else if (command.Contains("!update"))
                    {
                        
                    }else if (command.Contains("!refresh"))
                    {
                        appDataAnalyzer.RefreshGameList();
+                       var notification = new Notification
+                       {
+                           Title = "SteamCLI",
+                           Body = $"Refreshed game list"
+                       };
+                       await manager.ShowNotification(notification);
                    }else if (command.Contains("!reload"))
                    {
                        appDataAnalyzer.AnalizeJson();
+                       var notification = new Notification
+                       {
+                           Title = "SteamCLI",
+                           Body = $"Reloaded game list"
+                       };
+                       await manager.ShowNotification(notification);
                    }
                    else if (command.Contains("!quit"))
                    {
@@ -64,6 +126,15 @@ namespace SteamCli {
             }
             */
             appDataAnalyzer.RefreshGameList();
+           }
+           private static void ManagerOnNotificationDismissed(object? sender, NotificationDismissedEventArgs e)
+           {
+               Console.WriteLine($"Notification dismissed: {e.Reason}");
+           }
+
+           private static void ManagerOnNotificationActivated(object? sender, NotificationActivatedEventArgs e)
+           {
+               Console.WriteLine($"Notification activated: {e.ActionId}");
            }
       }
 };
--- SteamCli/SteamInteractions.cs
+++ SteamCli/SteamInteractions.cs
@@ -8,11 +8,23 @@ public class SteamInteractions
     {
         ProcessStartInfo processStartInfo = new ProcessStartInfo();
         processStartInfo.FileName = "nohup";
-        processStartInfo.Arguments = "steam steam://rungameid/"+game.appid +" > "+game.appid+".txt";
+        processStartInfo.Arguments = "steam -no-browser steam://rungameid/"+game.appid +" > "+game.appid+".txt";
         processStartInfo.RedirectStandardOutput = true;
         //processStartInfo.UseShellExecute = true;
         Process.Start(processStartInfo);
         //Process.Start("nohup","steam steam://rungameid/"+game.appid +" > "+game.appid+".txt");
         Console.WriteLine("nohup steam steam://rungameid/"+game.appid +" > "+game.name+game.appid+".txt");
+    }
+
+    public void RunSteam()
+    {
+        // +open steam://open/minigameslist
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        processStartInfo.FileName = "nohup";
+        processStartInfo.Arguments = "steam -no-browser +open steam://open/minigameslist > steam.txt";
+        processStartInfo.RedirectStandardOutput = true;
+        //processStartInfo.UseShellExecute = true;
+        Process.Start(processStartInfo);
+        //Process.Start("nohup","steam steam://rungameid/"+game.appid +" > "+game.appid+".txt");
     }
 }
